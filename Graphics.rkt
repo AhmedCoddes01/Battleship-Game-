36d24480402999471d639f7c65ba814492ca5f71
Ahmed <ahmedcodes01@gmail.com>
2026-06-07 22:33:31 +0100

    Add files via upload

---
+++ Graphics.rkt
@@ -0,0 +1,191 @@
+#lang racket
+(require dyoo-while-loop)
+
+(require "functions.rkt" racket/gui)
+
+(range 40 440 40)
+(range 0 400 40)
+
+(define state 0)
+(define button_state 0)
+(define coords(for*/vector ([i (range 520 900 40)] [j (range 40 420 40)]) (vector i j)))
+
+(define draw-grid (λ (x-start y-start x-extent y-extent player) (sleep/yield 0.001)
+                    (cond ((equal? player 1)(send dc draw-text "Player 🧍" x-start (- y-start 20)))
+                          ((equal? player 2)(send dc draw-text "Player 🤖" x-start (- y-start 20))))
+                    (for* ([i (range x-start x-extent 40)] [j (range y-start y-extent 40)]) (send dc draw-rectangle i j 40 40))
+                    (for ([i  (range y-start y-extent 40) ] [j (build-string 10 (lambda (i) (integer->char (+ i 97))))]) (send dc draw-text (string j) (- x-start 20) i))
+                    (for ([i  (range x-start x-extent 40) ] [j (build-list 10 (lambda (x) (+ x 1)))]) (send dc draw-text (number->string j) i y-extent))))
+(define draw-ship(λ (x-start y-start x-extent y-extent direction) (sleep/yield 0.001)(cond ((equal? direction "east")(send dc set-brush "black" 'transparent)(send dc set-pen "red" 5 'solid)(send dc draw-rectangle x-start y-start x-extent y-extent))
+                                                                        ((equal? direction "south")(send dc set-brush "black" 'transparent)(send dc set-pen "red" 5 'solid)(send dc draw-rectangle x-start (- y-start (- x-extent 40)) y-extent x-extent))
+                                                                        ((equal? direction "north")(send dc set-brush "black" 'transparent)(send dc set-pen "red" 5 'solid)(send dc draw-rectangle x-start y-start y-extent x-extent))
+                                                                        ((equal? direction "west")(send dc set-brush "black" 'transparent)(send dc set-pen "red" 5 'solid)(send dc draw-rectangle x-start y-start x-extent y-extent))
+                                                                        ((equal? direction "east")(send dc set-brush "black" 'transparent)(send dc set-pen "red" 5 'solid)(send dc draw-rectangle (- x-start (- x-extent 40)) y-start x-extent y-extent))
+                                                                        )))
+
+;deletes all items in panel2 and creates new ones
+(define murder(λ ()
+                        (send panel2 delete-child button)
+                        (send panel2 delete-child choice)
+                        (send panel2 delete-child choice2)
+                         (send text-field set-label "hello")
+                        (set! button_state 1)
+                         ))
+(define frame (new frame% [label "Example"]
+                   [alignment '(left top)]
+                   
+                   [min-width 1550]	 
+   	 	[min-height 730]
+                [stretchable-width #f]
+                [stretchable-height #f]))
+(define panel (new panel%  [parent frame]
+                   [alignment '(left top)]
+                   
+                 ))
+(define panel2 (new horizontal-panel%  [parent frame]
+              
+                   [min-height 50]
+     [min-width 100]
+     [stretchable-width #t]	 
+     [stretchable-height #f]
+                 ))
+(define my-canvas%
+  (class canvas% 
+    (define/override (on-event mouse-event) (cond ((equal? (send mouse-event get-event-type) 'right-down)
+                                                   (send dc draw-ellipse (send mouse-event get-x) (send mouse-event get-y) 20 20))
+                                                  ((equal? (send mouse-event get-event-type) 'left-down) (send panel set-cursor (make-object cursor% 'hand))(play-sound "explosion.wav" #t)(for ([i coords])
+                                                  (cond    ((and (equal? state 0)(< (send mouse-event get-x) (+ (vector-ref i 0) 40)) (> (send mouse-event get-x) (vector-ref i 0))  
+                                                        (> (send mouse-event get-y) (vector-ref i 1))   (< (send mouse-event get-y) (+ (vector-ref i 1) 40)))
+                                                            (set! state 1)
+                                                            (send dc set-text-foreground "orange red")
+                                                            (send dc set-font (make-font #:size 20 #:family 'swiss #:weight 'normal 	#:underlined? #f))
+                                                            (send dc draw-text "💥" (vector-ref i 0) (vector-ref i 1)))
+                                                           ) ))
+                                                  (else (send panel set-cursor (make-object cursor% 'arrow)))
+                                                  ))
+    (super-new)))
+
+(define x(new my-canvas% [parent panel]
+    
+     [paint-callback
+      (lambda (canvas dc)
+        (send dc set-brush (new brush% [color "white"]))
+        (send dc set-pen "black" 0.5 'solid)
+        (send dc set-background "red")
+        (send dc set-font (make-font #:size 11 #:family 'modern #:weight 'normal))
+        (draw-grid 40 40 440 440 1)
+        (draw-grid 520 40 900 440 2)
+        (send dc set-font (make-font #:size 20 #:family 'swiss #:weight 'normal #:underlined? #f))
+        (send dc set-text-mode 'solid)
+        (send dc set-text-background "medium sea green")
+        (send dc set-text-foreground "white")
+        (send dc set-text-mode 'transparent)
+        (send dc set-text-foreground "orange red")
+        (send dc set-font (make-font #:size 15 #:family 'modern #:weight 'normal))
+        (send dc set-text-foreground "black")
+        (send dc set-pen "black" 0.5 'solid)
+        ;player ships
+        (send dc set-pen "red" 5 'solid)
+        (send dc draw-rectangle 40 460 (ship-cumulative carrier) 40)
+        (send dc draw-text "Carrier" 50 470)
+        (send dc draw-rectangle 40 510 (ship-cumulative battleship) 40)
+        (send dc draw-text "battleship" 50 520)
+        (send dc draw-rectangle 40 560 (ship-cumulative cruiser) 40)
+        (send dc draw-text "cruiser" 50 570)
+        (send dc draw-rectangle 40 610 (ship-cumulative destroyer) 40)
+        (send dc set-font (make-font #:size 10 #:family 'modern #:weight 'normal))
+        (send dc draw-text "destroyer" 45 620)
+        (send dc set-font (make-font #:size 15 #:family 'modern #:weight 'normal))
+        ;computers ships
+        (send dc set-pen "blue" 5 'solid)
+        (send dc draw-rectangle 520 460 (ship-cumulative carrier) 40)
+        (send dc draw-text "Carrier" 530 470)
+        (send dc draw-rectangle 520 510 (ship-cumulative battleship) 40)
+        (send dc draw-text "battleship" 530 520)
+        (send dc draw-rectangle 520 560 (ship-cumulative cruiser) 40)
+        (send dc draw-text "cruiser" 530 570)
+        (send dc draw-rectangle 520 610 (ship-cumulative destroyer) 40)
+        (send dc set-font (make-font #:size 10 #:family 'modern #:weight 'normal))
+        (send dc draw-text "destroyer" 525 620)
+        (send dc set-font (make-font #:size 15 #:family 'modern #:weight 'normal))
+        )])
+  )
+
+(define ship_choices ship-list)
+(define choice(new choice% [parent panel2]
+       [label  ""]
+       [choices ship-list-strings]
+       [min-width 50]	 
+   	 	[min-height 30]
+       	[stretchable-width #f]	 
+   	 	[stretchable-height #t]
+       
+       ))
+(define choice2(new choice% [parent panel2]
+       [label  ""]
+       [choices '("north" "east" "south" "west")]
+       [min-width 50]	 
+   	 	[min-height 30]
+       	[stretchable-width #f]	 
+   	 	[stretchable-height #t]
+       
+       ))
+(define text-field (new text-field%
+                        (label "text  ")
+                        (parent panel2)
+                        [stretchable-width #f]
+                   
+                        (init-value "")))
+
+(define button (new button%
+                    (parent panel2)
+                    (label "place")
+                    (callback (lambda (button event) (cond ((equal? button_state 0)
+                                                            (cond
+                                                              ((equal? (send choice get-string-selection) "carrier")
+                                                               (send choice delete (send choice get-selection))(draw-ship
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 1)
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 0)
+                                                             (ship-cumulative carrier) square-size (send choice2 get-string-selection)))
+
+                                                              ((equal? (send choice get-string-selection) "battleship")
+                                                            (send choice delete (send choice get-selection))
+                                                            (send choice set-selection (random (send choice get-number)))
+                                                            (draw-ship
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 1)
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 0)
+                                                             (ship-cumulative battleship) square-size (send choice2 get-string-selection)))
+
+                                                              ((equal? (send choice get-string-selection) "cruiser")
+                                                            (send choice delete (send choice get-selection))
+                                                            (send choice set-selection (random (send choice get-number)))
+                                                            (draw-ship
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 1)
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 0)
+                                                             (ship-cumulative cruiser) square-size (send choice2 get-string-selection)))
+
+                                                              ((equal? (send choice get-string-selection) "submarine")
+                                                            (send choice delete (send choice get-selection))
+                                                            (send choice set-selection (random (send choice get-number)))
+                                                            (draw-ship
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 1)
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 0)
+                                                             (ship-cumulative submarine) square-size (send choice2 get-string-selection)))
+
+                                                              ((equal? (send choice get-string-selection) "destroyer")
+                                                            (send choice delete (send choice get-selection))
+                                                            (send choice set-selection (random (send choice get-number)))
+                                                            (draw-ship
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 1)
+                                                             (list-ref (return-input-int-draw (string (string-ref (send text-field get-value) 0)) (string->number (string (string-ref (send text-field get-value) 1)))) 0)
+                                                             (ship-cumulative destroyer) square-size (send choice2 get-string-selection)))
+                                                           )
+                                                                                   ((equal? button_state 1))))))))
+(filter (λ (x) (not (equal? x #\space))) (string->list (send text-field get-value)))
+;(string-ref (list->string (filter (λ (x) (not (equal? x #\space))) (string->list (send text-field get-value)))) 0)
+(define text (new text%))
+(send text insert "Editor Canvas")
+;(send editor-canvas set-editor text)
+(define dc (send x get-dc))
+(send frame show #t)
+(send text get-text)
---
+++ functions.rkt
@@ -0,0 +1,249 @@
+#lang racket
+(provide (all-defined-out))
+
+(require racket/trace)
+
+;; Variables
+(define empty-cell 0)
+(define occupied-cell 1)
+(define destroyed-cell 2)
+
+(define square-size 40)
+
+(define active-player null)
+(set! active-player "Player 1")
+
+(define game-state null)
+(set! game-state 0)
+
+; Grid setup
+(define P1-GRID (for/vector ([i 10]) (make-vector 10)))
+(define P1-GRID-COORDS (for*/vector ([i 10] [j 10]) (vector i j)))
+
+(define P2-GRID (for/vector ([i 10]) (make-vector 10)))
+(define P2-GRID-COORDS (for*/vector ([i 10] [j 10]) (vector i j)))
+
+
+(define grid-ht-p1 (hash
+                 "A" (vector-ref P1-GRID 0)
+                 "B" (vector-ref P1-GRID 1)
+                 "C" (vector-ref P1-GRID 2)
+                 "D" (vector-ref P1-GRID 3)
+                 "E" (vector-ref P1-GRID 4)
+                 "F" (vector-ref P1-GRID 5)
+                 "G" (vector-ref P1-GRID 6)
+                 "H" (vector-ref P1-GRID 7)
+                 "I" (vector-ref P1-GRID 8)
+                 "J" (vector-ref P1-GRID 9)))
+
+(define grid-ht-p2 (hash
+                 "A" (vector-ref P2-GRID 0)
+                 "B" (vector-ref P2-GRID 1)
+                 "C" (vector-ref P2-GRID 2)
+                 "D" (vector-ref P2-GRID 3)
+                 "E" (vector-ref P2-GRID 4)
+                 "F" (vector-ref P2-GRID 5)
+                 "G" (vector-ref P2-GRID 6)
+                 "H" (vector-ref P2-GRID 7)
+                 "I" (vector-ref P2-GRID 8)
+                 "J" (vector-ref P2-GRID 9)))
+
+; Ship setup
+(struct ship (name length P1-position P2-position P1-state P2-state cumulative) #:mutable)
+
+(define carrier (ship "carrier" 5 (list '() '() '() '() '() ) (list '() '() '() '() '() ) 0 0 (* 40 5)))
+(define battleship (ship "battleship" 4 (list '() '() '() '() ) (list '() '() '() '() ) 0 0 (* 40 4)))
+(define cruiser (ship "cruiser" 3 (list '() '() '() ) (list '() '() '() ) 0 0 (* 40 3)))
+(define submarine (ship "submarine" 3 (list '() '() '() ) (list '() '() '() ) 0 0 (* 40 3)))
+(define destroyer (ship "destroyer" 2 (list '() '() ) (list '() '() ) 0 0 (* 40 2)))
+
+(define ship-list (list carrier battleship cruiser submarine destroyer))
+(define ship-list-strings (for/list ([i ship-list]) (ship-name i)))
+
+  
+(define active-ships-p1 null)
+(set! active-ships-p1 (list))
+
+(define active-ships-p2 null)
+(set! active-ships-p2 (list))
+
+(define destroyed-ships-p1 null)
+(set! destroyed-ships-p1 (list))
+
+(define destroyed-ships-p2 null)
+(set! destroyed-ships-p2 (list))
+
+; Grid Functions
+(define grid-flatten (lambda (grid) (flatten (for*/list ([i (range 0 10)]) (vector->list (list-ref (vector->list grid) i))))))
+
+(define grid-list (lambda (grid) (hash->list grid #t)))
+
+(define grid-keys (lambda (grid) (hash-keys grid #t)))
+
+(define get-row-whole (lambda (y grid) (hash-ref grid y)))
+
+(define get-row-index (for*/vector ([i (range 0 10)]) (vector i 0)))
+
+(define get-active-grid (lambda (x)
+                          (cond
+                            ([equal? x "Player 1"] (displayln "Player 1") P1-GRID)
+                            ([equal? x "Player 2"] (displayln "Player 2") P2-GRID)
+                            )))
+
+(define get-active-ht (lambda (x)
+                        (cond
+                          ([equal? x "Player 1"] grid-ht-p1)
+                          ([equal? x "Player 2"] grid-ht-p2)
+                          )))
+
+(define set-state-grid (lambda (co-ord state grid-hash)
+                         (let ([y (first co-ord)]
+                               [x (second co-ord)])
+                           (vector-set! (hash-ref grid-hash y) (- x 1) state))))
+
+(define get-state-grid (lambda (co-ord grid)
+                         (let ([y (first co-ord)]
+                               [x (second co-ord)])
+                         (vector-ref (hash-ref grid y) (- x 1)))))
+
+(define return-input-int (lambda (y x index)
+                           (cond
+                             ([equal? index "x"]
+                              (cond
+                                ([equal? x 10] (+ x 1))
+                                (else x)))
+                             ([equal? index "y"]
+                              (cond
+                                ([equal? y "J"] (+ 2 (index-of (grid-keys grid-ht-p1) y)))
+                                (else (+ 1 (index-of (grid-keys grid-ht-p1) y))))
+                              )
+                             )))
+
+(define return-input-int-draw (lambda (y x)
+                                (list (* 40 (return-input-int y x "y")) (* 40 (return-input-int y x "x")))))
+
+; Ship Functions
+(define set-ship-position (lambda (ship-name y x direction lst)
+                            (cond
+                              ([equal? direction "west"]
+                                (cond
+                                  ([equal? (length lst) (ship-length ship-name)] (cond
+                                                                                   ([equal? active-player "Player 1"] (set-ship-P1-position! ship-name (reverse lst)))
+                                                                                   ([equal? active-player "Player 2"] (set-ship-P2-position! ship-name (reverse lst)))))
+                                  ([< x 1] "Can not place ship here")
+                                  (else (set-ship-position ship-name y (+ x 1) "west" (cons (list y x) lst)))
+                                  )
+                                )
+                              ([equal? direction "east"]
+                                (cond
+                                  ([equal? (length lst) (ship-length ship-name)] (cond
+                                                                                   ([equal? active-player "Player 1"] (set-ship-P1-position! ship-name (reverse lst)))
+                                                                                   ([equal? active-player "Player 2"] (set-ship-P2-position! ship-name (reverse lst)))))
+                                  ([> x 10] "Can not place ship here")
+                                  (else (set-ship-position ship-name y (- x 1) "east" (cons (list y x) lst)))
+                                  )
+                               )
+                              ([equal? direction "south"]
+                                (cond
+                                  ([equal? (length lst) (ship-length ship-name)] (cond
+                                                                                   ([equal? active-player "Player 1"] (set-ship-P1-position! ship-name (reverse lst)))
+                                                                                   ([equal? active-player "Player 2"] (set-ship-P2-position! ship-name (reverse lst)))))
+                                  ([equal? y "A"] (set-ship-P1-position! ship-name (reverse (cons (list y x) lst))))
+                                  (else (set-ship-position ship-name (list-ref (grid-keys (get-active-ht active-player)) (- (index-of (grid-keys (get-active-ht active-player)) y) 1)) x "south" (cons (list y x) lst)))
+                                  )
+                               )
+                              ([equal? direction "north"]
+                                (cond
+                                  ([equal? (length lst) (ship-length ship-name)] (cond
+                                                                                   ([equal? active-player "Player 1"] (set-ship-P1-position! ship-name (reverse lst)))
+                                                                                   ([equal? active-player "Player 2"] (set-ship-P2-position! ship-name (reverse lst)))))
+                                  ([equal? y "J"] (set-ship-P1-position! ship-name (reverse (cons (list y x) lst))))
+                                  (else (set-ship-position ship-name (list-ref (grid-keys (get-active-ht active-player)) (+ (index-of (grid-keys (get-active-ht active-player)) y) 1)) x "north" (cons (list y x) lst)))
+                                  )
+                                )
+                              )))
+
+(define add-ship-list (lambda (ship-name active-list)
+                           (set! active-list (cons ship-name active-list))))
+
+(define remove-ship-list (lambda (ship-name active-list)
+                           (set! active-list (remove ship-name active-list))))
+
+(define all-active-pos (lambda (actv-plyr)
+                         (cond
+                         ([equal? actv-plyr "Player 1"] (for/list ([i active-ships-p1]) (ship-P1-position i)))
+                         ([equal? actv-plyr "Player 2"] (for/list ([i active-ships-p2]) (ship-P2-position i)))
+                         )
+                         )
+  )
+
+; Draw Functions
+(define set-ship-position-grid (lambda (ship-name position-arg)
+                             (for ([i position-arg])
+                               (set-state-grid i occupied-cell (get-active-ht active-player)))))
+
+(define draw-ship-to-grid (lambda (ship-name y x direction)
+                            (set-ship-position ship-name y x direction '())
+                            (change-game-state)
+                            (cond
+                              ([and (equal? active-player "Player 1") (ship-collision y x (flatten (all-active-pos "Player 1")))] (set! active-ships-p1 (cons ship-name active-ships-p1)) (set-ship-position-grid ship-name (ship-P1-position ship-name)) (set-ship-P1-state! ship-name 1) (get-active-grid active-player) (change-active-player) P1-GRID)
+                              ([and (equal? active-player "Player 2") (ship-collision y x (flatten (all-active-pos "Player 2")))] (set! active-ships-p2 (cons ship-name active-ships-p2)) (set-ship-position-grid ship-name (ship-P2-position ship-name)) (set-ship-P2-state! ship-name 1) (get-active-grid active-player) (change-active-player) P2-GRID))
+                            )
+  )
+
+(define ship-collision (lambda (y x pos-lst)
+                         (cond
+                           ([empty? pos-lst] #t)
+                           ([and (equal? y (car pos-lst)) (equal? x (cadr pos-lst))] #f)
+                           (else (ship-collision y x (cddr pos-lst)))
+                           )
+                         )
+  )
+
+(trace ship-collision)
+
+; Game State Functions
+(define change-game-state (lambda ()
+                            (cond
+                              ([and (equal? (length active-ships-p1) 5) (equal? (length active-ships-p2) 5)](set! game-state (+ game-state 1)))
+                              ([or (equal? (length destroyed-ships-p1) 5) (equal? (length destroyed-ships-p2) 5)] (set! game-state (+ game-state 1)) (won-game-state active-player))
+                              )))
+
+(define won-game-state (lambda (player) (cond
+                                    ([equal? game-state 2] (displayln "Game Won") player))))
+(define change-active-player (lambda ()
+                               (cond
+                                 ([equal? active-player "Player 1"] (set! active-player "Player 2"))
+                                 ([equal? active-player "Player 2"] (set! active-player "Player 1"))
+                                 )))
+
+; Shoot Functions
+(define shoot (lambda (y x player)
+                (cond
+                  ([equal? player "Player 1"]
+                   (let ([active-hash grid-ht-p1]
+                         [active-lst (flatten (all-active-pos "Player 1"))])
+                     (set-state-grid (list y x) destroyed-cell active-hash)
+                     (ship-hit-check y x active-lst)
+                     ))
+                   
+                  ([equal? player "Player 2"]
+                   (let ([active-hash grid-ht-p2]
+                         [active-lst (flatten (all-active-pos "Player 2"))])
+                     (set-state-grid (list y x) destroyed-cell active-hash)
+                     (ship-hit-check y x active-lst)
+                     ))
+                  )
+                )
+  )
+
+(define ship-hit-check (lambda (y x pos-lst)
+                         (cond
+                           ([empty? pos-lst] "Miss")
+                           ([and (equal? y (car pos-lst)) (equal? x (cadr pos-lst))] "Ship Hit")
+                           (else (ship-hit-check y x (cddr pos-lst)))
+                           )
+                         )
+  )
+
+; Computer/Player 2 Functions
